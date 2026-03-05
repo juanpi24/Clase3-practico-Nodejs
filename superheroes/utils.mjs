e61493a8939d7c8190fbfa2be54eb314eb891a2f
Juan Pablo Millicay <juan22milli@gmail.com>
2026-03-05 08:50:28 -0300

    Crear función agregarSuperheroes

--- superheroes/utils.mjs
+++ superheroes/utils.mjs
@@ -71,3 +71,40 @@ export function leerSuperheroes(ruta) {
     return superHeroes;
 }
 
+//Nueva función para agregar superhéroes
+export function agregarSuperheroes(rutaOriginal, rutaNuevos) {
+     const datosOriginales = fs.readFileSync(rutaOriginal, "utf8");
+     const datosNuevos = fs.readFileSync(rutaNuevos, "utf8");
+
+     const superheroesOriginales = JSON.parse(datosOriginales);
+     const nuevosSuperheroes = JSON.parse(datosNuevos);
+
+     // Convertir los nuevos superhéroes a instancias de Superhéroe
+     const instanciaNuevos = nuevosSuperheroes.map(
+         (hero) =>
+             new Superheroe(
+                 hero.id,
+                 hero.nombreSuperheroe,
+                 hero.nombreReal,
+                 hero.nombreSociedad,
+                 hero.edad,
+                 hero.planetaOrigen,
+                 hero.debilidad,
+                 hero.poder,
+                 hero.habilidadEspecial,
+                 hero.aliado,
+                 hero.enemigo,
+             ),
+     );
+
+//     // Combinar listas
+     const listaActualizada = [...superheroesOriginales, ...instanciaNuevos];
+
+//     // Guardar la lista actualizada
+     fs.writeFileSync(
+         rutaOriginal,
+         JSON.stringify(listaActualizada, null, 2),
+         "utf8",
+     );
+     console.log("¡¡Lista de Superhéroes Actualizada con Éxito!!");
+ }
